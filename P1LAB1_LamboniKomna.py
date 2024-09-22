dedb25adabe38ca199187d88842339f69581d7c4
lambonik2162 <148191167+lambonik2162@users.noreply.github.com>
2024-09-22 14:39:02 -0400

    Add files via upload
    First submission.

---
+++ P1LAB1_LamboniKomna.py
@@ -0,0 +1,8 @@
+                    # Komna Lamboni
+                    # 09/22/2024
+                    # P1LAB1
+                    # Using Python's input and print functions to complete a fillout form for the new students in CTI-110
+first_name = input('Enter your first name:',)
+last_name = input('Enter your last name:',)
+print('Hello,',first_name, last_name +'!', 'Welcome to CTI-110')
+
